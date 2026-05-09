6a0319a57c4114b276e9a93f3c89049e64397edd
nunomeira8 <nunomeira8@gmail.com>
2026-05-09 09:54:11 +0100

    Renaming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center pt-16">
       <h1 className="text-4xl font-bold">
-        Sushi Counter 🍣
+        Sushi League
       </h1>
 
       <h2 className="mt-2 text-lg text-center">
-        All you can eat version!
+        All you can (b)eat version!
       </h2>
     </main>
   )
