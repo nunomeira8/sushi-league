15f416092317797e61a70901f60f32bf814bab3b
nunomeira8 <nunomeira8@gmail.com>
2026-05-15 20:57:56 +0100

    Player Time
    Fix vercel fail.
    
    33

--- src/app/room/[code]/game/page.tsx
+++ src/app/room/[code]/game/page.tsx
@@ -126,11 +126,9 @@ export default function GamePage() {
   }
 
   async function giveUp() {
-    const playerId = getPlayerId();
+    if (!room || !room.started_at) return;
 
-    if (!room.started_at) {
-      return;
-    }
+    const playerId = getPlayerId();
 
     const startedAt = new Date(room.started_at).getTime();
     const now = Date.now();
